c9c85ff51a1ac0f589da33f504f9ccf833da8b8c
Mikkel Sommer Kristensen <mikkelsk64@hotmail.com>
2020-11-01 13:56:41 +0100

    Changed: Trees should now look like trees. Changed: Tree height tweeked. Chagned: Removed lodes from the default biome.

--- Assets/Project/Scripts/Structure.cs
+++ Assets/Project/Scripts/Structure.cs
@@ -6,27 +6,52 @@ public static class Structure
 {
     public static void MakeTree(int minTrunkHeight, int maxTrunkHeight, Vector3 position, Queue<VoxelMod> queue)
     {
-        int height = (int)(maxTrunkHeight * Noise.Get2DPerlin(new Vector2(position.x, position.z), 500, 3f));
+        float rawTrunkHeight = Mathf.Lerp(minTrunkHeight, maxTrunkHeight, Noise.Get2DPerlin(new Vector2(position.x, position.z), 500, 3f));
 
-        if (height < minTrunkHeight)
-            height = minTrunkHeight;
+        int trunkHeight = (int)rawTrunkHeight;
+
+        if (trunkHeight < minTrunkHeight)
+            trunkHeight = minTrunkHeight;
 
         // create the trunk
-        for (int i = 1; i < height; i++)
+        for (int i = 1; i <= trunkHeight; i++)
         {
             queue.Enqueue(new VoxelMod(new Vector3(position.x, position.y + i, position.z), 6));
         }
 
+        // add a leaf block on top of the trunk
+        queue.Enqueue(new VoxelMod(new Vector3(position.x, position.y + trunkHeight + 1, position.z), 11));
+
+
         for (int x = -2; x <= 2; x++)
         {
-            for (int y = height - 3; y < height; y++)
+            for (int y = trunkHeight - 2; y <= trunkHeight; y++)
             {
                 for (int z = -2; z <= 2; z++)
                 {
-                    // don't replace the trunk
-                    if (x != 0 || z != 0)
+                    // if x and z is 0, continue to the next loop (to not replace the trunk)
+                    if (x == 0 && z == 0)
+                        continue;
+
+                    // add leaves around the trunk, until y is equal to trunk height
+                    if (y < trunkHeight)
                     {
                         queue.Enqueue(new VoxelMod(new Vector3(position.x + x, position.y + y, position.z + z), 11));
+                    }
+                    // add leaves around the top of the trunk
+                    else if (y == trunkHeight)
+                    {
+                        // make the top a bit smaller than the layer below
+                        if (x >= -1 && x <= 1 && z >= -1 && z <= 1)
+                        {
+                            queue.Enqueue(new VoxelMod(new Vector3(position.x + x, position.y + y, position.z + z), 11));
+
+                            // if either x or z is 0, add leaves above the block too, makes the "+" shape at the top of the tree
+                            if (x == 0 || z == 0)
+                            {
+                                queue.Enqueue(new VoxelMod(new Vector3(position.x + x, position.y + y + 1, position.z + z), 11));
+                            }
+                        }
                     }
                 }
             }
